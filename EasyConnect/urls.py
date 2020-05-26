cb4c4a4aff8f0b3f77ba5dec79657b3023d9516b
dathmar <thatguy349@aol.com>
2020-05-26 14:35:49 -0500

    Updated index.html with Django type coding for URL reference.

--- EasyConnect/urls.py
+++ EasyConnect/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('our-story/', views.our_story, name='our-story'),
-    path('faqs/', views.faq, name='FAQ'),
-    path('faq/', views.faq, name='FAQ'),
+    path('faqs/', views.faq, name='faq'),
+    path('faq/', views.faq, name='faq'),
     path('privacy-policy/', views.privacy_policy, name='privacy-policy'),
     path('connect-with-a-doctor/', views.connect, name='connect-with-a-doctor'),
     path('connect/', views.connect, name='connect-with-a-doctor'),
