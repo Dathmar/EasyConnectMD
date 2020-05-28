f2a72b4be3819c8d46c0587b707affb9d3a8c9ef
dathmar <thatguy349@aol.com>
2020-05-28 14:07:45 -0500

    Moved reference templates into new folder.

--- EasyConnect/views.py
+++ EasyConnect/views.py
@@ -1,5 +1,4 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -8,23 +7,23 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Create your views here.
 def index(request):
-    return render(request, 'EasyConnect/index.html')
+    return render(request, 'EasyConnect/templates/template_reference/index.html')
 
 
 def our_story(request):
-    return render(request, 'EasyConnect/our-story.html')
+    return render(request, 'EasyConnect/templates/template_reference/our-story.html')
 
 
 def faq(request):
-    return render(request, 'EasyConnect/faq.html')
+    return render(request, 'EasyConnect/templates/template_reference/faq.html')
 
 
 def privacy_policy(request):
-    return render(request, 'EasyConnect/privacy-policy.html')
+    return render(request, 'EasyConnect/templates/template_reference/privacy-policy.html')
 
 
 def connect(request):
-    return render(request, 'EasyConnect/connect.html')
+    return render(request, 'EasyConnect/templates/template_reference/connect.html')
 
 
 def video_chat(request, sid):
