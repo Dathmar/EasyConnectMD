220fceb6da36aae5c6c222117bb5edfad6105c7c
dathmar <thatguy349@aol.com>
2020-05-28 10:57:12 -0500

    Updated templates to use header_footer.html to test extending template for nav bar and footer.

--- EasyConnect/models.py
+++ EasyConnect/models.py
@@ -2,19 +2,6 @@ from django.db import models
 from EasyConnect.choices import STATE_CHOICES
 
 # Create your models here.
-"""
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-"""
-
-
 class Patient(models.Model):
     # page 1
     first_name = models.CharField(max_length=200)
@@ -81,5 +68,3 @@ class Video_Chat(models.Model):
 
     def __str__(self):
         return self.sid
-
-
--- EasyConnect/views.py
+++ EasyConnect/views.py
@@ -1,9 +1,10 @@
+from django.http import Http404
 from django.shortcuts import render
-from django.http import HttpResponse
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 # Create your views here.
 def index(request):
