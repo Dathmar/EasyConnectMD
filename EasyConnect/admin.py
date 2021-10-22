a69c0751a10e753b8c4a63544f2b577ffad74f3e
Asher Danner <adanner@ets.org>
2021-10-22 07:41:03 -0500

    Fixed a spacing issue on index.html.  Added payments to admin

--- EasyConnect/admin.py
+++ EasyConnect/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Affiliate, Coupon
+from .models import Affiliate, Coupon, Patient, Payment
 
 # Register your models here.
 admin.site.register(Affiliate)
 admin.site.register(Coupon)
+admin.site.register(Patient)
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'patient', 'status', 'create_datetime', 'update_datetime']
+    list_filter = ['status', 'create_datetime', 'update_datetime']
